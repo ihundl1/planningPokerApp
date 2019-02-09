b53d4c39acb26906884012327a032109ac09248e
Isabella <isabella.n.hundley@gmail.com>
2019-02-08 21:27:43 -0600

    Functioning home page w/ size cards

---
+++ src/app/selected-card/selected-card.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-card',
+  templateUrl: './selected-card.page.html',
+  styleUrls: ['./selected-card.page.scss'],
+})
+export class SelectedCardPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
