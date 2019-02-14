cd55f380acd489ff3fa0df9fca561f663dad3d54
Isabella <isabella.n.hundley@gmail.com>
2019-02-13 19:12:02 -0600

    Light grey background for selected list items

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class CardService {
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
-  fibSeq = ['1', '2', '3', '5', '8', '13'];
+  fibonacci = ['1', '2', '3', '5', '8', '13'];
   dogs = ['Christmas', 'Ripley', 'German Shepherd', 'Juno', 'Great Dane'];
   animals = ['rabbit', 'dog', 'deer', 'tiger', 'elephant', 'blue whale'];
   selection = 'sizes';
-  options = ['sizes', 'fibSeq'];
+  options = ['sizes', 'fibonacci'];
 
   constructor() { }
 
@@ -17,7 +17,7 @@ export class CardService {
     if (this.selection == 'sizes') {
       return this.sizes;
     } else {
-      return this.fibSeq;
+      return this.fibonacci;
     }
   }
 }
