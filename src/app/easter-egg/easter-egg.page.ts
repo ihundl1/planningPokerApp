7e914d42ed2694e859e81aec9659b808b7d24f02
Isabella <isabella.n.hundley@gmail.com>
2019-02-15 18:46:54 -0600

    Easter egg finished.

--- src/app/easter-egg/easter-egg.page.ts
+++ src/app/easter-egg/easter-egg.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-easter-egg',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./easter-egg.page.scss'],
 })
 export class EasterEggPage implements OnInit {
-
-  constructor() { }
+  pics = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
+          '14', '15']
+          
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this.router.navigate(['home']);
   }
 
 }
